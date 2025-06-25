c709a1ba1f36dc366d1dbb1d6b59a501e42e8ce4
Rohan Poudel <np05cp4s240029@iic.edu.np>
2025-06-25 23:10:35 +0545

    feat: integrate all modules into main application

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,16 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BookModule } from './book/book.module';
+import { UserModule } from './user/user.module';
+import { TransactionModule } from './transaction/transaction.module';
+import { MemberModule } from './member/member.module';
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [ BookModule],
+  imports: [ ConfigModule.forRoot({
+    isGlobal:true,
+  }),BookModule, UserModule, TransactionModule, MemberModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
