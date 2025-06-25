cba051bc730f9c8198c14d686e3618713ddaec25
Rohan Poudel <np05cp4s240029@iic.edu.np>
2025-06-25 23:09:53 +0545

    feat: add user registration and management module

---
+++ src/user/user.controller.ts
@@ -0,0 +1,18 @@
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { UserCreateDto } from './dto/user.dto';
+import { UserService } from './user.service';
+
+@Controller('user')
+export class UserController {
+    constructor(private userService:UserService){}
+
+    @Post('adduser')
+    addUser(@Body() rules:UserCreateDto){
+        return this.userService.createUser(rules);
+    }
+
+    @Get()
+    findAllUsers(){
+        return this.userService.findAllUsers()
+    }
+}
---
+++ src/user/user.service.ts
@@ -0,0 +1,56 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../../prisma/prisma.service';
+import { UserCreateDto } from './dto/user.dto';
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class UserService {
+    constructor(private prisma:PrismaService){}
+
+    async createUser(data:UserCreateDto){
+        const hashed =  await bcrypt.hash(data.password, 10);
+        try{
+            const user = await this.prisma.user.create({
+                data:{
+                    name:data.name,
+                    email:data.email,
+                    password:hashed
+                },
+                select:{
+                    name:true,
+                    email:true,
+                    password:true
+                }
+            });
+            return {
+                messagge:"book created successpully",
+                data:user
+                
+            }
+        }catch(e){
+            console.log(e)
+        }
+    }
+
+    async findAllUsers(){
+        return this.prisma.user.findMany({
+            select:{
+                    name:true,
+                    email:true,
+                    password:true
+            }
+        })
+    }
+    async findByEmail(email:string){
+        return this.prisma.user.findUnique({
+            where:{
+                email:email
+            },
+            select:{
+                name:true,
+                email:true,
+                password:true
+            }
+        });
+    }
+}
