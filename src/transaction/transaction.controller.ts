13d227f74096b0b399aa7205437dd7f2c2478eb3
Rohan Poudel <np05cp4s240029@iic.edu.np>
2025-06-26 14:34:25 +0545

    Implement transaction controller logic

---
+++ src/transaction/transaction.controller.ts
@@ -0,0 +1,39 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Request, Req } from '@nestjs/common';
+import { TransactionService } from './transaction.service';
+import { CreateTransactionDto } from './dto/create-transaction.dto';
+import { UpdateTransactionDto } from './dto/update-transaction.dto';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { AuthRequest } from 'src/auth/auth-request.interface';
+
+@UseGuards(JwtAuthGuard)
+@Controller('transaction')
+export class TransactionController {
+  constructor(private transactionService: TransactionService) {}
+
+  @Post('add')
+  create(@Body() createTransactionDto: CreateTransactionDto, @Req() req:AuthRequest) {
+    const librarianId = req.user.userId;
+    return this.transactionService.createTransaction(createTransactionDto,librarianId);
+  }
+
+  @Get()
+  findAll(@Req() req:AuthRequest) {
+    const librarianId = req.user.userId;
+    return this.transactionService.findAll(librarianId);
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.transactionService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateTransactionDto: UpdateTransactionDto) {
+    return this.transactionService.update(+id, updateTransactionDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.transactionService.remove(+id);
+  }
+}
