ed6f5f536f33166d947d50e3cc86d64711128c41
Rohan Poudel <np05cp4s240029@iic.edu.np>
2025-06-26 14:35:20 +0545

    Modify main.ts to adjust application bootstrap or config

--- src/main.ts
+++ src/main.ts
@@ -5,16 +5,20 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+    app.enableCors({
+    origin: true, 
+    credentials: true,
+  });
   
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
     forbidNonWhitelisted: true,
     transform: true,
     transformOptions: {
-      enableImplicitConversion: true, // This converts strings to numbers automatically
+      enableImplicitConversion: true, 
     },
   }));
   
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen( 3000, '0.0.0.0');
 }
 bootstrap();
